ef8e7ed5fbfe41baa49199fd4d15b5b7ed9b541f
fanyinghao <fanyinghao@gmail.com>
2020-11-28 14:06:35 +0800

    fix memo bug

--- dna/memo.go
+++ dna/memo.go
@@ -1,25 +1,13 @@
 package dna
 
 import (
-	"fmt"
-
+	"github.com/blocktree/bitshares-adapter/encoding"
 	"github.com/denkhaus/bitshares/config"
-	"github.com/denkhaus/bitshares/types"
 )
 
 //Decrypt calculates a shared secret by the receivers private key
 //and the senders public key, then decrypts the given memo message.
-func Decrypt(msg, fromPub, toPub string, nonce uint64, wif string) (string, error) {
-
-	if len(msg) == 0 || len(fromPub) == 0 || len(toPub) == 0 {
-		return "", nil
-	}
-
-	if len(wif) == 0 {
-		return "", fmt.Errorf("wif cannot be empty")
-	}
-
-	var buf types.Buffer
+func Decrypt(msg []byte, fromPub, toPub string, nonce uint64, wif string) (string, error) {
 	ret := config.FindByID(ChainIDDNA)
 	if ret == nil {
 		config.Add(config.ChainConfig{
@@ -31,33 +19,5 @@ func Decrypt(msg, fromPub, toPub string, nonce uint64, wif string) (string, erro
 	}
 	config.SetCurrent(ChainIDDNA)
 
-	from, err := types.NewPublicKeyFromString(fromPub)
-	if err != nil {
-		return "", fmt.Errorf("NewPublicKeyFromString: %v", err)
-	}
-	to, err := types.NewPublicKeyFromString(toPub)
-	if err != nil {
-		return "", fmt.Errorf("NewPublicKeyFromString: %v", err)
-	}
-
-	buf.FromString(msg)
-
-	memo := types.Memo{
-		From:    *from,
-		To:      *to,
-		Message: buf,
-		Nonce:   types.UInt64(nonce),
-	}
-
-	priv, err := types.NewPrivateKeyFromWif(wif)
-	if err != nil {
-		return "", fmt.Errorf("NewPrivateKeyFromWif: %v", err)
-	}
-
-	m, err := memo.Decrypt(priv)
-	if err != nil {
-		return "", fmt.Errorf("Decrypt: %v", err)
-	}
-
-	return m, nil
+	return encoding.Decrypt(msg, fromPub, toPub, nonce, wif)
 }
--- dna/memo_test.go
+++ dna/memo_test.go
@@ -1,6 +1,10 @@
 package dna
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/blocktree/bitshares-adapter/types"
+)
 
 func TestDecrypt(t *testing.T) {
 	type args struct {
@@ -16,21 +20,45 @@ func TestDecrypt(t *testing.T) {
 		want    string
 		wantErr bool
 	}{
+		// {
+		// 	name: "test1",
+		// 	want: "hello boy",
+		// 	args: args{
+		// 		msg:     "17d0ac3874548d7c4ef56236698d719e",
+		// 		nonce:   5577006791947779410,
+		// 		fromPub: "DNA6fpcoYK72BxsYYRwcBEPGuVoGhy2Yki2YCNfnCZCYxL5xp56Hh",
+		// 		toPub:   "DNA6icdz8dWibXRz8PcDn9RMupFkPbwHQ4toHxP8UmLm2hDtMHUKr",
+		// 		wif:     "",
+		// 	},
+		// },
+		// {
+		// 	name: "test2",
+		// 	want: "hellowrd111122223333",
+		// 	args: args{
+		// 		msg:     "5a9436f43ba1299afca79ebbf9698b08ace004eba9c17a789d158b74d195ed84",
+		// 		nonce:   5405926024387204188,
+		// 		fromPub: "DNA8Xi4HwBaUAkYD5wT7jRaAS1B2NUqw8rzE29Vdaww5zvtsse7tW",
+		// 		toPub:   "DNA6fpcoYK72BxsYYRwcBEPGuVoGhy2Yki2YCNfnCZCYxL5xp56Hh",
+		// 		wif:     "",
+		// 	},
+		// },
 		{
-			name: "test1",
-			want: "hello boy",
+			name: "test3",
+			want: "123456789012",
 			args: args{
-				msg:     "17d0ac3874548d7c4ef56236698d719e",
-				nonce:   5577006791947779410,
+				msg:     "d08f99ecaab4bb6eab51bd39d3c65462",
+				nonce:   13260572831089785859,
 				fromPub: "DNA6fpcoYK72BxsYYRwcBEPGuVoGhy2Yki2YCNfnCZCYxL5xp56Hh",
-				toPub:   "DNA6icdz8dWibXRz8PcDn9RMupFkPbwHQ4toHxP8UmLm2hDtMHUKr",
+				toPub:   "DNA6fpcoYK72BxsYYRwcBEPGuVoGhy2Yki2YCNfnCZCYxL5xp56Hh",
 				wif:     "",
 			},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := Decrypt(tt.args.msg, tt.args.fromPub, tt.args.toPub, tt.args.nonce, tt.args.wif)
+			var buf types.Buffer
+			buf.FromString(tt.args.msg)
+			got, err := Decrypt(buf, tt.args.fromPub, tt.args.toPub, tt.args.nonce, tt.args.wif)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("Decrypt() error = %v, wantErr %v", err, tt.wantErr)
 				return
